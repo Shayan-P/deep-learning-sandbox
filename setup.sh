a443ca8c6dc05706c4ea81dc4433691a52c50667
Shayan-P <shayanpardis82@gmail.com>
2025-02-23 05:38:56 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-set -e
+# Function to execute command and handle errors
+run_cmd() {
+    if ! "$@"; then
+        echo "Error occurred while running: $*"
+        return 1
+    fi
+}
 
 # setting up the server
 echo "THIS IS MEANT TO BE RUN ON A NEW SERVER. DON'T RUN IT ON YOUR LOCAL MACHINE."
@@ -8,7 +14,7 @@ echo "ARE YOU SURE YOU WANT TO CONTINUE? (y/n)"
 read -r answer
 if [ "$answer" != "y" ]; then
     echo "exiting"
-    exit 1
+    return 1
 fi
 
 USE_CONDA=false
@@ -16,46 +22,43 @@ USE_CONDA=false
 if [ "$USE_CONDA" = true ]; then
     if ! command -v conda &> /dev/null; then
         # Conda not found, proceed with installation
-
         echo "installing miniconda"
         CONDA_PATH="/workspace/miniconda3"
 
-        mkdir -p $CONDA_PATH
-        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $CONDA_PATH/miniconda.sh
-        bash $CONDA_PATH/miniconda.sh -b -u -p $CONDA_PATH
-        rm $CONDA_PATH/miniconda.sh
-        $CONDA_PATH/bin/conda init bash
-        source ~/.bashrc # just to make sure the path is set and conda is available
+        run_cmd mkdir -p $CONDA_PATH
+        run_cmd wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $CONDA_PATH/miniconda.sh
+        run_cmd bash $CONDA_PATH/miniconda.sh -b -u -p $CONDA_PATH
+        run_cmd rm $CONDA_PATH/miniconda.sh
+        run_cmd $CONDA_PATH/bin/conda init bash
+        run_cmd source ~/.bashrc # just to make sure the path is set and conda is available
 
-
-        conda create -n ml python=3.10
+        run_cmd conda create -n ml python=3.10
     fi
 
     echo "activating conda environment"
-    conda activate ml
+    run_cmd conda activate ml
 fi
 
+run_cmd apt update
+
 echo "installing pipx"
-apt update
+run_cmd apt install pipx
 
 echo "installing apt dependencies"
-apt install tmux
-apt install htop
-apt install nvtop
-apt install nano
+run_cmd apt install tmux
+run_cmd apt install htop
+run_cmd apt install nvtop
+run_cmd apt install nano
 
 echo "installing poetry"
-pipx install poetry
-pipx ensurepath
-source ~/.bashrc # just to make sure the path is set
+run_cmd pipx install poetry
+run_cmd pipx ensurepath
+run_cmd source ~/.bashrc # just to make sure the path is set
 
 echo "installing dependencies with poetry"
-# Configure poetry to not create virtual environment and use conda instead
-# poetry config virtualenvs.create false
-# poetry config virtualenvs.in-project false
-poetry config virtualenvs.create true
-poetry config virtualenvs.in-project true
-poetry install
-
+run_cmd poetry config virtualenvs.create true
+run_cmd poetry config virtualenvs.in-project true
+run_cmd poetry install
 
 echo "Setup complete!"
+return 0
