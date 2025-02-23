a8b2cf0d2ae39ddb54ae254dc21dc1d1da22d45c
Shayan-P <shayanpardis82@gmail.com>
2025-02-23 05:29:41 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -11,23 +11,29 @@ if [ "$answer" != "y" ]; then
     exit 1
 fi
 
-if ! command -v conda &> /dev/null; then
-    # Conda not found, proceed with installation
+USE_CONDA=false
 
-    echo "installing miniconda"
-    CONDA_PATH="/workspace/miniconda3"
+if [ "$USE_CONDA" = true ]; then
+    if ! command -v conda &> /dev/null; then
+        # Conda not found, proceed with installation
 
-    mkdir -p $CONDA_PATH
-    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $CONDA_PATH/miniconda.sh
-    bash $CONDA_PATH/miniconda.sh -b -u -p $CONDA_PATH
-    rm $CONDA_PATH/miniconda.sh
-    $CONDA_PATH/bin/conda init bash
-    source ~/.bashrc # just to make sure the path is set and conda is available
+        echo "installing miniconda"
+        CONDA_PATH="/workspace/miniconda3"
+
+        mkdir -p $CONDA_PATH
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $CONDA_PATH/miniconda.sh
+        bash $CONDA_PATH/miniconda.sh -b -u -p $CONDA_PATH
+        rm $CONDA_PATH/miniconda.sh
+        $CONDA_PATH/bin/conda init bash
+        source ~/.bashrc # just to make sure the path is set and conda is available
+
+
+        conda create -n ml python=3.10
+    fi
+
+    echo "activating conda environment"
+    conda activate ml
 fi
-
-echo "activating conda environment"
-conda create -n ml python=3.10
-conda activate ml
 
 echo "installing pipx"
 apt update
@@ -45,8 +51,10 @@ source ~/.bashrc # just to make sure the path is set
 
 echo "installing dependencies with poetry"
 # Configure poetry to not create virtual environment and use conda instead
-poetry config virtualenvs.create false
-poetry config virtualenvs.in-project false
+# poetry config virtualenvs.create false
+# poetry config virtualenvs.in-project false
+poetry config virtualenvs.create true
+poetry config virtualenvs.in-project true
 poetry install
 
 
